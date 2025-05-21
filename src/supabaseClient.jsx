c36035a258c681e92c6590c8f142e9a02f6f136a
reedscar <rscarbor27@gmail.com>
2025-05-21 15:27:58 -0400

    Update supabaseClient.jsx

--- src/supabaseClient.jsx
+++ src/supabaseClient.jsx
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://pewfrdimuznfxactxteb.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBld2ZyZGltdXpuZnhhY3R4dGViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NjkxNTYsImV4cCI6MjA2MzM0NTE1Nn0.hE9wzM6aFYIoKG-_YESdMJaQnokSXAvTngM8Mxyfcs0'
+const supabaseUrl = 'YOUR URL'
+const supabaseKey = 'YOUR KEY'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
